728882492b47d20921ebbb39360f60b344f6fddc
mohammedsalah19 <themsm1922011@yahoo.com>
2018-11-23 18:18:20 +0200

    second commit

---
+++ src/app/mediaApp.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mw-media-item',
+  templateUrl: './media-item.html',
+  styleUrls: [ './style.css' ]
+})
+export class mediaComponent  {
+ }
